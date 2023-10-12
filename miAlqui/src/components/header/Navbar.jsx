88b0f871bc30c96527bcae56fb46bc70b67f742c
unknown <soledad110203@gmail.com>
2023-10-12 07:14:52 -0300

    aplicar css a mi pagina de inicio

--- miAlqui/src/components/header/Navbar.jsx
+++ miAlqui/src/components/header/Navbar.jsx
@@ -1,17 +1,23 @@
 import React from "react";
+import './navbar.css'
 
 export const Navbar = () => {
   return (
-    <nav class="nav nav-masthead justify-content-center float-md-end">
-      <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#">
+    <nav className="nav nav-masthead justify-content-center float-md-end">
+      <div className="container-fluid">
+      <a  href="Mialqui.jsx" class="navbar-brand logo tex-color">
+        <h1>mi<span>Alqui</span>
+        </h1></a>
+      <a className="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#">
         Home
       </a>
-      <a class="nav-link fw-bold py-1 px-0" href="#">
+      <a className="nav-link fw-bold py-1 px-0" href="#">
         Features
       </a>
-      <a class="nav-link fw-bold py-1 px-0" href="#">
+      <a className="nav-link fw-bold py-1 px-0" href="#">
         Contact
       </a>
+     </div>
     </nav>
   );
 };
--- miAlqui/src/components/header/header.jsx
+++ miAlqui/src/components/header/header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Navbar } from "./Navbar";
+import './header.css'
 
 export const Header = () => {
   return (
-    <header class="mb-auto">
-      <div>
+    <header className="mb-auto">
+      <div className="contenedorHeader  cover-container">
+      
         <Navbar />
       </div>
     </header>
